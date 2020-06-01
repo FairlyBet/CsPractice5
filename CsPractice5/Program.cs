20f724da8e78dd217b87d66cc1697beb94ff3734
Александр Загаревский <fairlybet@gmail.com>
2020-06-01 14:06:55 +0300

    m

--- CsPractice5/Program.cs
+++ CsPractice5/Program.cs
@@ -8,15 +8,10 @@ namespace CsPractice5
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
-        [STAThread]
+       
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Console.WriteLine("Hello, world!");
         }
     }
 }
